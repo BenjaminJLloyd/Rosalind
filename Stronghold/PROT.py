b9ef977fbf1ea5f7e596298ac04b0e11d55112c0
Benjamin Lloyd <benjaminjlloyd@icloud.com>
2025-08-07 16:28:46 +0100

    dumbass didnt tell me how to handle stop codons

---
+++ Stronghold/PROT.py
@@ -0,0 +1,35 @@
+def translate_rna(rna_string):
+    rna_codon_table = {
+        'UUU': 'F', 'UUC': 'F', 'UUA': 'L', 'UUG': 'L',
+        'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S',
+        'UAU': 'Y', 'UAC': 'Y', 'UAA': '*', 'UAG': '*',
+        'UGU': 'C', 'UGC': 'C', 'UGA': '*', 'UGG': 'W',
+        'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
+        'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+        'CAU': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q',
+        'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
+        'AUU': 'I', 'AUC': 'I', 'AUA': 'I', 'AUG': 'M',
+        'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
+        'AAU': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',
+        'AGU': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R',
+        'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V',
+        'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+        'GAU': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
+        'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G',
+    }
+
+    protein_sequence = ""
+
+    for i in range(0, len(rna_string), 3):
+        codon = rna_string[i:i + 3]
+        if len(codon) == 3:
+            amino_acid = rna_codon_table.get(codon)
+            if amino_acid == '*':
+                break
+            protein_sequence += amino_acid
+    return protein_sequence
+
+
+s = ""
+protein = translate_rna(s)
+print(protein)
